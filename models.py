f13e227b944d43ee815145cfcf31d8f5b51335a6
JoshuaBalles <joshuaballes07@gmail.com>
2024-04-17 12:46:53 +0800

    Added config.py and models.py for user database and secret key

---
+++ models.py
@@ -0,0 +1,13 @@
+# models.py (do not change/remove this comment)
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
